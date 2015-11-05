5185ed9333d644fb5665134572373d2210ed4ef7
Mausam Jain <mj92android@gmail.com>
2015-11-05 22:18:37 +0530

    Update pos_tagging.py

--- pos_tagging.py
+++ pos_tagging.py
@@ -1,5 +1,4 @@
-# First, record is fetched from the database in line 13 and then reviews are then worked upon
-# The text review sentences are tokenized one-by-one and finally all the tokens are printed
+# The result of get_tokens() function is used and POS tagging is done here
 
 import MySQLdb
 import nltk
@@ -17,18 +16,22 @@ def pos(token):
   	 return tagged
 
 if __name__ == "__main__":
-	
+
+# Connect to database
 db = MySQLdb.connect(host="localhost", # your host, usually localhost
         user="root", # your username
         passwd="password", # your password
         db ="testdb1") # name of the data base
 cur = db.cursor()
 
+# Query statement
 cur.execute("SELECT reviewText FROM  reviewElectronicsHelpful WHERE prodid='0528881469'")
 i = 0
 for row in cur.fetchall() :
     print "\n\n","Review = : ",i," Data = : ",row
-	token=get_tokens(row[0])
+	
+	# Function call
+	token = get_tokens(row[0])
 	print ""
 	print("tokens = %s") %(token)
 	
