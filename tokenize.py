64f590159954e31a38eac2992929b1b1627944c6
Mausam Jain <mj92android@gmail.com>
2015-11-05 22:32:46 +0530

    Update tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -11,23 +11,24 @@ def get_tokens(row):
     token = word_tokenize(row)
     return token
 
+# Beginning of main
 if __name__ == "__main__":
 
-# Connect to database
-db = MySQLdb.connect(host="localhost", # your host, usually localhost
-        user="root", # your username
-        passwd="password", # your password
-        db ="testdb1") # name of the data base
-cur = db.cursor()
+	# Connect to database
+	db = MySQLdb.connect(host="localhost", # your host, usually localhost
+	        user="root", # your username
+	        passwd="password", # your password
+	        db ="testdb1") # name of the data base
+	cur = db.cursor()
 
-# Query statement
-cur.execute("SELECT reviewText FROM  reviewElectronicsHelpful WHERE prodid='0528881469'")
-i = 0
-for row in cur.fetchall() :
-    print "\n\n","Review = : ",i," Data = : ",row
+	# Query statement
+	cur.execute("SELECT reviewText FROM  reviewElectronicsHelpful WHERE prodid='0528881469'")
+	i = 0
+	for row in cur.fetchall() :
+	    print "\n\n","Review = : ",i," Data = : ",row
     
-    # Function call
-	token = get_tokens(row[0])
-	print ""
-	print("tokens = %s") %(token)
-	i += 1
+    	# Function call
+		token = get_tokens(row[0])
+		print ""
+		print("tokens = %s") %(token)
+		i += 1
