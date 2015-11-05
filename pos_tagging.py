11e408b9f04487b2bf28e46c3dfe83bd44487008
Mausam Jain <mj92android@gmail.com>
2015-11-05 22:31:41 +0530

    Update pos_tagging.py

--- pos_tagging.py
+++ pos_tagging.py
@@ -15,28 +15,29 @@ def pos(token):
 	 tagged = nltk.pos_tag(token)
   	 return tagged
 
+# Beginning of main
 if __name__ == "__main__":
 
-# Connect to database
-db = MySQLdb.connect(host="localhost", # your host, usually localhost
-        user="root", # your username
-        passwd="password", # your password
-        db ="testdb1") # name of the data base
-cur = db.cursor()
+	# Connect to database
+	db = MySQLdb.connect(host="localhost", # your host, usually localhost
+	        user="root", # your username
+	        passwd="password", # your password
+	        db ="testdb1") # name of the data base
+	cur = db.cursor()
 
-# Query statement
-cur.execute("SELECT reviewText FROM  reviewElectronicsHelpful WHERE prodid='0528881469'")
-i = 0
-for row in cur.fetchall() :
-    print "\n\n","Review = : ",i," Data = : ",row
+	# Query statement
+	cur.execute("SELECT reviewText FROM  reviewElectronicsHelpful WHERE prodid='0528881469'")
+	i = 0
+	for row in cur.fetchall() :
+    		print "\n\n","Review = : ",i," Data = : ",row
 	
-	# Function call
-	token = get_tokens(row[0])
-	print ""
-	print("tokens = %s") %(token)
+		# Function call
+		token = get_tokens(row[0])
+		print ""
+		print("tokens = %s") %(token)
+		
+		# Function POS() is called
+		tagged = pos(token)
+		print("POS = %s") %(tagged)
 	
-	# Function POS() is called
-	tagged = pos(token)
-	print("POS = %s") %(tagged)
-	
-	i += 1
+		i += 1
